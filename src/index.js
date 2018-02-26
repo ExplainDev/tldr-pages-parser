2f4dbd227737681bae228232e9a8bd0c85f75e54
Eddie Ramirez <djedi.r@gmail.com>
2018-02-25 21:11:40 -0800

    Tests

--- src/index.js
+++ src/index.js
@@ -1,26 +1,105 @@
-var marked = require('marked');
-var fs = require('fs');
-var path = require('path');
+const marked = require('marked');
+const fs = require('fs');
+const path = require('path');
+const _ = require('underscore');
 
-const manPagesPath = path.join('vendor', 'tldr', 'pages', 'common');;
-
+const pagesPath = path.join('vendor', 'tldr', 'pages');
 const platforms = [
-  { id: 0, name: 'generic', directory: 'common' },
-  { id: 1, name: 'linux', directory: 'linux' },
-  { id: 2, name: 'osx', directory: 'osx' },
-  { id: 3, name: 'windows', directory: 'windows' }
+  { id: 1, name: 'generic', directory: 'common',  },
+  { id: 2, name: 'linux', directory: 'linux' },
+  { id: 3, name: 'osx', directory: 'osx' },
+  { id: 4, name: 'windows', directory: 'windows' }
 ];
 
-platforms.
-fs.readdir(manPagesPath, (error, items) => {
-    fs.readFile(path.join(manPagesPath, 'ansible.md'), 'utf8', (error, data) => {
-      var tokens = marked.lexer(data);
-      tokens = tokens.filter((token, idx) => {
-        if (token.type === 'heading' || token.type === 'text' || token.type === 'paragraph')
-          return true;
-        else
-          return false;
-      })
-      console.log(tokens);
+const findPages = () => {
+  const promises = [];
+  platforms.forEach((platform) => {
+    const p = new Promise((resolve, reject) => {
+      fs.readdir(path.join(pagesPath, platform.directory), (err, files) => {
+        if (err) reject(err);
+        const pages = files.map(file => ({
+          platformId: platform.id,
+          name: file.substr(0, file.indexOf('.')),
+          file,
+          path: path.join(pagesPath, platform.directory),
+        }));
+        resolve(pages);
+      });
     });
-});
+    promises.push(p);
+  });
+  return Promise.all(promises).then((platformPages) => {
+    return platformPages.reduce((prev, curr) => {
+      return prev.concat(curr);
+    });
+  }, (error) => {
+    console.log(error);
+  }).catch(() => {
+    console.log('Error');
+  });
+}
+
+const readPages = async (pages) => {
+  const promises = [];
+  pages.forEach((page) => {
+    const p = new Promise((resolve, reject) => {
+      fs.readFile(path.join(page.path, page.file), 'utf8', (err, data) => {
+        if (err) reject(err);
+        resolve({ ...page, contents: data });
+      });
+    });
+    promises.push(p);
+  });
+  return Promise.all(promises).then((readFiles) => {
+    return readFiles;
+  }, (error) => {
+    console.log(error);
+  }).catch(() => {
+    console.log('error');
+  });
+};
+
+const parsePages = (pages) => {
+  return pages.map((page) => {
+    const lexer = new marked.Lexer();
+    let tokens = lexer.lex(page.contents);
+    tokens = tokens.filter(token => token.type === 'heading' || token.type === 'paragraph' || token.type === 'text');
+    return {
+      ...page,
+      tokens,
+    };
+  });
+};
+
+
+const getPrograms = (pages) => {
+  return pages.map((page, idx) => {
+    const cliName = _.chain(page.tokens)
+      .find({ type: 'heading' })
+      .pick('text')
+      .value()
+      .text;
+    return {
+      id: idx + 1,
+      cliName,
+      name: page.name,
+      platformId: page.platformId,
+    };
+  });
+};
+
+/*
+const getCommands = (programs) => {
+
+}
+*/
+
+const process = async () => {
+  let pages = await findPages();
+  pages = await readPages(pages);
+  pages = parsePages(pages);
+  let programs = getPrograms(pages);
+  console.log(programs);
+}
+
+process();
